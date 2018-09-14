0e1f2245a4d584f3acab77c0ee42caf597160c31
Andrey Popov <andpop@mail.ru>
2018-09-15 00:42:11 +0300

    Added admin assets.

--- app/Product.php
+++ app/Product.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
+    protected $fillable = ['name', 'category_id', 'price', 'photo_file', 'description'];
+
     /**
      * Получить запись о категории, к которой принадлежит товар
      * @return \Illuminate\Database\Eloquent\Relations\HasOne
@@ -22,4 +25,47 @@ class Product extends Model
         return $this->hasMany(Order::class);
     }
 
+    public static function add($fields)
+    {
+        $product = new static;
+        $product->fill($fields);
+        $product->save();
+
+        return $product;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
+    }
+
+    public function remove()
+    {
+//        удалить фото товара
+        $this->delete();
+    }
+
+    public function uploadPhoto($photo) {
+        Storage::delete('uploads/'.$this->photo_file);
+        $filename = str_random(10) . '.' . $photo->extension;
+        $photo->saveAs('uploads', $filename);
+        $this->photo_file = $filename;
+        $this->save();
+    }
+
+    public function setCategory($id)
+    {
+        if ($id == null) {return;};
+        $this->category_id = $id;
+        $this->save();
+    }
+
+    public function getPhoto()
+    {
+        if ($this->photo_file == null)  {
+            return '/img/no-image.jpg';
+        }
+        return '/uploads/'.$this->photo_file;
+    }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,22 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles([
+    'resources/admin/bootstrap/css/bootstrap.min.css',
+    'resources/admin/font-awesome/4.5.0/css/font-awesome.min.css',
+    'resources/admin/ionicons/2.0.1/css/ionicons.min.css',
+    'resources/admin/dist/css/AdminLTE.min.css',
+    'resources/admin/dist/css/skins/_all-skins.min.css'
+], 'public/css/admin.css');
+mix.scripts([
+    'resources/admin/plugins/jQuery/jquery-2.2.3.min.js',
+    'resources/admin/bootstrap/js/bootstrap.min.js',
+    'resources/admin/plugins/slimScroll/jquery.slimscroll.min.js',
+    'resources/admin/plugins/fastclick/fastclick.js',
+    'resources/admin/assets/dist/js/app.min.js',
+    'resources/admin/dist/js/demo.js'
+], 'public/js/admin.js');
+
