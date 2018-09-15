555219a7867f6c438efd7bf67f92401dba4ea9b2
Andrey Popov <andpop@mail.ru>
2018-09-15 22:05:29 +0300

    Added CRUD for categories.

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('admin.layout')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Добавить категорию
+                <small>приятные слова..</small>
+            </h1>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <!-- Default box -->
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Меняем категорию</h3>
+                    @include('admin.errors')
+                </div>
+                <div class="box-body">
+                    {{Form::open(['route'=>['categories.update',$category->id], 'method'=>'put'])}}
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="category-name">Название</label>
+                            <input type="text" class="form-control" id="category-name" name="name" placeholder=""
+                                   value="{{$category->name}}">
+                            <label for="category-description">Описание</label>
+                            <input type="text" class="form-control" id="category-description" placeholder="" name="description"
+                                   value="{{$category->description}}">
+                        </div>
+                    </div>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                    <button class="btn btn-default">Назад</button>
+                    <button class="btn btn-warning pull-right">Изменить</button>
+                </div>
+                <!-- /.box-footer-->
+                {{Form::close()}}
+            </div>
+            <!-- /.box -->
+
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+    Route::get('/', 'DashboardController@index');
+    Route::resource('/categories', 'CategoriesController');
+
+});
